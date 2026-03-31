9fdecb936cd0ba1101dba8a441462d4177346282
Andre Carvalhais <carvalhais@live.com>
2026-03-31 20:01:45 -0300

    wip: basic home page layout

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,13 @@
+import { readFileSync } from "node:fs";
+const site = JSON.parse(readFileSync("./src/_data/site.json", "utf-8"));
+
 export default function (eleventyConfig) {
 	eleventyConfig.setInputDirectory('src');
 	eleventyConfig.setOutputDirectory('dist');
+	eleventyConfig.addPassthroughCopy({
+		"src/assets/css": site.path.prefix + site.path.assets + "/css",
+		"src/assets/images": site.path.prefix + site.path.assets + "/images",
+		"src/assets/js": site.path.prefix + site.path.assets + "/js",
+		"src/assets/webfonts": site.path.prefix + site.path.assets + "/webfonts"
+	});
 }
